041755228c49d84835aff1345c236efffb2f4677
KEGAIMMERSION <dmitriydimon623@gmail.com>
2024-05-19 14:04:22 +0300

    добавление ссылки в cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,19 +1,21 @@
-function cors(req, res, next) {
-    const { origin } = req.headers
-    if (allowedCors.includes(origin)) { 
-        res.header('Access-Control-Allow-Origin', origin)
-        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    }
-    next()
-} 
-
 const allowedCors = [
     'https://practicum.yandex.ru',
     'https://students-projects.ru',
     'http://localhost:3000',
     'http://localhost:3001',
-    'mongodb://localhost:27017/Pindie'
+    'mongodb://localhost:27017/Pindie',
+    "https://frontend-pasma.nomoredomainswork.ru"
   ]
+
+function cors(req, res, next) {
+    const { origin } = req.headers
+
+    if (allowedCors.includes(origin)) { 
+        res.header('Access-Control-Allow-Origin', origin)
+    }
+        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization")
+        next()
+} 
 
 module.exports = cors
