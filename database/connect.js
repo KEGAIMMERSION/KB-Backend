1b9ad90775cbfe4e9478cdff476e6109f2ac3a9e
KEGAIMMERSION <dmitriydimon623@gmail.com>
2024-05-19 21:24:14 +0300

    коллекции для mongodb

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const DB_URL = 'mongodb://localhost:27017/Pindie'
+const DB_URL = 'mongodb://localhost:27017/pindie'
 
 async function connectToDatabase() {
   try {
