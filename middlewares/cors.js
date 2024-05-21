937794c338913c283b6c83d787a619fef7361a05
KEGAIMMERSION <dmitriydimon623@gmail.com>
2024-05-21 21:45:23 +0300

    1

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -8,12 +8,12 @@ const allowedCors = [
 function cors(req, res, next) {
     const { origin } = req.headers
 
-    if (allowedCors.includes(origin)) { 
+    if (allowedCors.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin)
-    }
         res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization")
-        next()
-} 
-
+        res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization')
+      }
+      next()
+    }
+    
 module.exports = cors
