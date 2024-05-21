3dbb5f1aa109be18457a76b389750de482b4ea8a
KEGAIMMERSION <dmitriydimon623@gmail.com>
2024-05-21 19:59:57 +0300

    enw

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,21 +1,28 @@
 const allowedCors = [
     'https://practicum.yandex.ru',
     'https://students-projects.ru',
-    'https://localhost:3000',
-    'https://localhost:3001',
+    'http://localhost:3000',
+    'http://localhost:3001',
     'mongodb://127.0.0.1:27017/pindie',
     "https://frontend-pasma.nomoredomainswork.ru"
-  ]
+];
 
 function cors(req, res, next) {
-    const { origin } = req.headers
+    const { origin } = req.headers;
 
-    if (allowedCors.includes(origin)) { 
-        res.header('Access-Control-Allow-Origin', origin)
+    if (allowedCors.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
     }
-        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization")
-        next()
-} 
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Access-Control-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
+        res.status(200).send();
+    } else {
+        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Access-Control-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
+        next();
+    }
+}
 
 module.exports = cors
