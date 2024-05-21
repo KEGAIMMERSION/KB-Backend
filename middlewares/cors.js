81c12ffa115c8e5e438eb68585b01dc534af7df8
KEGAIMMERSION <dmitriydimon623@gmail.com>
2024-05-21 23:38:06 +0300

    new

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -10,9 +10,9 @@ function cors(req, res, next) {
 
     if (allowedCors.includes(origin)) { 
         res.header('Access-Control-Allow-Origin', origin)
-    }
         res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
         res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization")
+    }
         next()
 } 
 
