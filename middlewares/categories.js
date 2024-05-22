4880dcd38e6008d028c5ef40be9ab48e9a06d2c2
KEGAIMMERSION <dmitriydimon623@gmail.com>
2024-05-22 17:22:57 +0300

    m

--- middlewares/categories.js
+++ middlewares/categories.js
@@ -60,6 +60,10 @@ const checkIsCategoryExists = async (req, res, next) => {
 }
 
 const checkIfCategoriesAvaliable = async (req, res, next) => {
+  if(req.isVoteRequest) {
+    next();
+    return;
+  } 
   if (!req.body.categories || req.body.categories.length === 0) {
     res.setHeader("Content-Type", "application/json")
         res.status(400).send(JSON.stringify({ message: "Выберите хотя бы одну категорию" }))
--- middlewares/games.js
+++ middlewares/games.js
@@ -20,10 +20,6 @@ const checkIsVoteRequest = async (req, res, next) => {
   if (Object.keys(req.body).length === 1 && req.body.users) {
     req.isVoteRequest = true;
   }
-  if(req.isVoteRequest) {
-    next()
-    return
-  } 
 }
 
 const createGame = async (req, res, next) => {
@@ -70,6 +66,10 @@ const deleteGame = async(req, res, next) => {
 }
 
 const checkEmptyFields = async(req, res, next) => { 
+  if(req.isVoteRequest) {
+    next();
+    return;
+  } 
   if(
     !req.body.title ||
     !req.body.description ||
