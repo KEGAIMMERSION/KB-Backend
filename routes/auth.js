b7d1d9cbce082d4b5ecc01da105f36fcda426360
KEGAIMMERSION <145719468+KEGAIMMERSION@users.noreply.github.com>
2024-05-26 18:38:38 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,5 +2,6 @@ const authRouter = require("express").Router()
 const { login }  = require("../controllers/auth")
 
 authRouter.post("/auth/login", login)
+authRouter.get("/me", checkAuth, sendMe)
 
 module.exports = authRouter
