e817b6ab7721a53ac95d91e09298550f9a29c6c0
KEGAIMMERSION <145719468+KEGAIMMERSION@users.noreply.github.com>
2024-05-26 17:29:22 +0300

    Update games.js

--- middlewares/games.js
+++ middlewares/games.js
@@ -1,18 +1,17 @@
 const games = require("../models/game")
 
 const findAllGames = async (req, res, next) => {
-  if(req.query["categories.name"]) { 
-    req.gamesArray = await games.findGameByCategory(req.query["categories.name"])
+  if (req.query["categories.name"]) {
+    req.gamesArray = await games.findGameByCategory(
+      req.query["categories.name"]
+    )
     next()
     return
   }
-  req.gamesArray = await games
-    .find({})
-    .populate("categories")
-    .populate({
-      path: "users",
-      select: "-password"
-    })
+  req.gamesArray = await games.find({}).populate("categories").populate({
+    path: "users",
+    select: "-password",
+  })
   next()
 }
 
