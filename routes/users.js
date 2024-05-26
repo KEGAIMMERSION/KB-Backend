9d641d8a9cdb3b91b815177368e584d2cb73a54c
KEGAIMMERSION <145719468+KEGAIMMERSION@users.noreply.github.com>
2024-05-26 17:57:26 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ usersRouter.get('/users', findAllUsers, sendAllUsers)
 usersRouter.get('/me', checkAuth, sendMe)
 usersRouter.post('/users', findAllUsers, checkIsUserExists, checkEmptyNameAndEmailAndPassword, checkAuth, hashPassword, createUser, sendUserCreated)
 usersRouter.get('/users/:id', findUserById, sendUserById)
-usersRouter.put("/users/:id", checkEmptyNameAndEmail, checkAuth, updateUser, sendUserUpdated)
+usersRouter.put("/users/:id", checkEmptyNameAndEmail, checkIsUserExists, checkAuth, updateUser, sendUserUpdated)
 usersRouter.delete("/users/:id", checkAuth, deleteUser, sendUserDeleted)
 
 module.exports = usersRouter
