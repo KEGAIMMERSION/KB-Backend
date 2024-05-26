659481f781faff313e50a13e98356701c452eecf
KEGAIMMERSION <145719468+KEGAIMMERSION@users.noreply.github.com>
2024-05-26 18:39:10 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const authRouter = require("express").Router()
 const { login }  = require("../controllers/auth")
+const { sendMe } = require("../controllers/users")
+const { checkAuth } = require("../middlewares/auth")
 
 authRouter.post("/auth/login", login)
 authRouter.get("/me", checkAuth, sendMe)
